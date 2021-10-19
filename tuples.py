b102a6a335dd3c50b7d219bb9e9b4dff1575d20a
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 10:56:47 +0300

    Creating tuples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+#tuples are like lists, but tuples are immutable
+#By immutable i  mean there values cannot be changed one they have been created
+
+tuple_a = ("a","b","c","d") # tuple of strings
+tuple_b = (1,2,3,4,5,6) # tuple of numbers
+tuple_c = (1,"west",34,"longitude") # mixed tuple
+tuple_d = tuple() #empty tuple
+
+print(tuple_d)
