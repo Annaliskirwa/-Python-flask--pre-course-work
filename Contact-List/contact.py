91ba5cbef669ebdeca54dd0e3099018d19bf0c97
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 21:39:04 +0300

    Instance variable

--- Contact-List/contact.py
+++ Contact-List/contact.py
@@ -1,18 +1,13 @@
 class Contact:
     """
-    Class that generates new instances of contacts
+    Class that generates new instances of contacts.
     """
 
-    pass
+    def __init__(self,first_name,last_name,phone_number,email):
 
-def __init__(self,first_name,last_name,phone_number,email):
+      # docstring removed for simplicity
 
-        '''
-        __init__ method that helps us define properties for our objects.
-
-        Args:
-            first_name: New contact first name.
-            last_name : New contact last name.
-            number: New contact phone number.
-            email : New contact email address.
-        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
