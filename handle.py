46cd2cfefc0235db6658b299f4fbc206007dda7e
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 19:51:59 +0300

    try and exception block to handle value  errors

--- handle.py
+++ handle.py
@@ -1,7 +1,16 @@
+#def get_age():
+    #print("How old are you ")
+    #age = int(input())
+    #return age
+
+#print(get_age())
+#accepts intergers, what if a user enters a string; gets a value error; exception handling
+
+#try/except block to handle errors
 def get_age():
     print("How old are you ")
-    age = int(input())
-    return age
-
-print(get_age())
-#accepts intergers, what if a user enters a string; exception handling
+    try:
+        age = int(input())
+        return age
+    except ValueError:
+        return "That was not a valid input"
