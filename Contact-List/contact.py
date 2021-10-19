768e96fff462ad213d120a95fa0dd93c23ce5403
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 21:33:57 +0300

    the __init__ method

--- Contact-List/contact.py
+++ Contact-List/contact.py
@@ -4,3 +4,15 @@ class Contact:
     """
 
     pass
+
+def __init__(self,first_name,last_name,phone_number,email):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New contact first name.
+            last_name : New contact last name.
+            number: New contact phone number.
+            email : New contact email address.
+        '''
