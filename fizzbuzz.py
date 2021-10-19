56dcf38b3dc64c55cb6f745903e5e59c105beacf
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 20:00:54 +0300

    fizzbuzz using for loop

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,8 +4,20 @@ def fizzbuzz(int):
             print("fizz")
 
         if i%5 == 0:
-                print ("buzz")
+            print ("buzz")
 
         if i%15 == 0:
-                    print("fizzbuzz")
+            print("fizzbuzz")
     return (fizzbuzz)
+#this one runs
+for fizzbuzz in range(51):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
