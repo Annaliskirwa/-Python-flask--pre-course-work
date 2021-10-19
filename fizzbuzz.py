24008a1a958b66cd7f143644f8d0938a3228f860
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 19:58:52 +0300

    Fizzbuzz solution in python

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz(int):
+    for i in range (0,100):
+        if i%3 == 0:
+            print("fizz")
+
+        if i%5 == 0:
+                print ("buzz")
+
+        if i%15 == 0:
+                    print("fizzbuzz")
+    return (fizzbuzz)
