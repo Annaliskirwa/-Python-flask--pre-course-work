04fcb04218ab1cfdd9dc91a7d784a9b2602cd8b2
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 20:10:44 +0300

    REversing the string using while loop

--- reverse.py
+++ reverse.py
@@ -38,3 +38,18 @@ def reverse_string(str):
 str = "JavaTpoint"    # Given String       
 print("The original string is: ",str)  
 print("The reverse string is",reverse_string(str)) # Function call  
+
+
+
+# Reverse string  
+# Using a while loop  
+  
+str = "JavaTpoint" #  string variable  
+print ("The original string  is : ",str)   
+reverse_String = ""  # Empty String  
+count = len(str) # Find length of a string and save in count variable  
+while count > 0:   
+    reverse_String += str[ count - 1 ] # save the value of str[count-1] in reverseString  
+    count = count - 1 # decrement index  
+print ("The reversed string using a while loop is : ",reverse_String)# reversed string  
+
