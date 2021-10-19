7d2b2909d2885e545fdf241b37041bc0b6e2f794
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 10:49:59 +0300

    Nested lists

--- lists.py
+++ lists.py
@@ -1,4 +1,6 @@
 list_a = ["a","b","c","d"] # list of strings
 list_b = [1,2,3,4,5,6] # list of numbers
 list_c = [1,"west",34,"longitude"] # mixed list
+
+list_d = [ ["a","b","c","d"],[1,2,3,4,5,6],[1,"west",34,"longitude"]] # nested list
 print (list_a)
