250fccf5adf26fa34a7f89fcf9a4283d085bf94c
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 11:04:19 +0300

    Accessing values in dictionary using keys

--- dictionary.py
+++ dictionary.py
@@ -7,3 +7,5 @@ my_dict = dict()
 
 # Creating a dictionary with keys and values
 my_cat = {'name':'Mr sniffles','age':18, 'color':'black'}
+cat_name = my_cat['name']
+print(cat_name) # 'Mr sniffles'
