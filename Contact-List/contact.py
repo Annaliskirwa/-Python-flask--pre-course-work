45273b9884d7d284ad46fc876fc70c7e9788584c
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 21:42:14 +0300

    It will create a class variable

--- Contact-List/contact.py
+++ Contact-List/contact.py
@@ -2,11 +2,13 @@ class Contact:
     """
     Class that generates new instances of contacts.
     """
-
+    #class variable
+    contact_list = [] #empty contact list
     def __init__(self,first_name,last_name,phone_number,email):
 
       # docstring removed for simplicity
 
+        #instance variables
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
