d0d352c22ca233a7499cba52d2d9675f4cb385d9
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 10:52:09 +0300

    Adding item in list using append

--- lists.py
+++ lists.py
@@ -5,4 +5,6 @@ list_c = [1,"west",34,"longitude"] # mixed list
 list_d = [ ["a","b","c","d"],[1,2,3,4,5,6],[1,"west",34,"longitude"]] # nested list
 
 list_a.extend(list_b)
+
+list_a.append("e")
 print (list_a)
