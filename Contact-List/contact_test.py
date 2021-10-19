a96387e7b9326553a618bf364b228108ebfb8bd4
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-20 00:18:42 +0300

    Test case to check if we can add multiple contacts

--- Contact-List/contact_test.py
+++ Contact-List/contact_test.py
@@ -40,6 +40,18 @@ class TestContact(unittest.TestCase):
         self.new_contact.save_contact() # saving the new contact
         self.assertEqual(len(Contact.contact_list),1)
 
+    # Items up here...
+
+    def test_save_multiple_contact(self):
+            '''
+            test_save_multiple_contact to check if we can save multiple contact
+            objects to our contact_list
+            '''
+            self.new_contact.save_contact()
+            test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
+            test_contact.save_contact()
+            self.assertEqual(len(Contact.contact_list),2)
+
 if __name__ == '__main__':
     unittest.main() #command line interface that collects all the tests methods and executes them.
 
