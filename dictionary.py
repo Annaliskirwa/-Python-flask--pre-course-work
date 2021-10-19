bc97d11ddc622b2d9fff2b3e5495932ea4353fc3
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 11:02:20 +0300

    Creating dictionaries in py

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+#Indexes in dictionaries are called keys, can be integers / strings
+#The values are called values
+
+# Creating empty dictionaries
+my_dict = {}
+my_dict = dict()
+
+# Creating a dictionary with keys and values
+my_cat = {'name':'Mr sniffles','age':18, 'color':'black'}
