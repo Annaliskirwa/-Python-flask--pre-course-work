d0722f2f98ac8a87de498a4a9254936fb7945776
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-20 00:29:17 +0300

    Code that makes the find user by number pass.

--- Contact-List/contact.py
+++ Contact-List/contact.py
@@ -30,5 +30,20 @@ class Contact:
         '''
 
         Contact.contact_list.remove(self)
-        
+    
+
+    @classmethod
+    def find_by_number(cls,number):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
 #new_contact.first_name
