9073e5670b1fc37b4bcbfcf26bbe2168679f0495
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 13:24:20 +0300

    slicing lists

--- slicing.py
+++ slicing.py
@@ -8,3 +8,8 @@ print(part_one)
 print(part_two)
 part_three = greetings[0:]
 print(part_three)
+
+#slicing lists in python
+number = [1,2,3,4,5,6,7,8,9]
+four_digits = number[:4]
+print(four_digits)
