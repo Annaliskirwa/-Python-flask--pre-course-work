df6e4b86c71a0d8e03c95e18ae438a7aab8396b3
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 10:57:52 +0300

    Using the tuple function to create tuples

--- tuples.py
+++ tuples.py
@@ -4,6 +4,6 @@
 tuple_a = ("a","b","c","d") # tuple of strings
 tuple_b = (1,2,3,4,5,6) # tuple of numbers
 tuple_c = (1,"west",34,"longitude") # mixed tuple
-tuple_d = tuple() #empty tuple
+tuple_d = tuple(tuple_a) #empty tuple
 
 print(tuple_d)
