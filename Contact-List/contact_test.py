7a0b3658bfb4fc27fb38bca1ac0a6ba614964708
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 22:08:23 +0300

    Using the unittest framework to do test cases in python

---
+++ Contact-List/contact_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from contact import Contact # Importing the contact class
+
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
