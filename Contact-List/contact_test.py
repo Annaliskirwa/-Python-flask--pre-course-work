1d155e5064f8deae338e1f33be6072016376e75d
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-20 00:02:35 +0300

    test if the objects are instantiated correctly

--- Contact-List/contact_test.py
+++ Contact-List/contact_test.py
@@ -9,3 +9,26 @@ class TestContact(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+# Items up here .......
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_contact.first_name,"James")
+        self.assertEqual(self.new_contact.last_name,"Muriuki")
+        self.assertEqual(self.new_contact.phone_number,"0712345678")
+        self.assertEqual(self.new_contact.email,"james@ms.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
