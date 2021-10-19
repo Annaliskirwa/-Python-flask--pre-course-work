1c56d21ee735e3418721a119faef48999e8ca9e9
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-19 20:13:32 +0300

    Reversing using reverse and join

--- reverse.py
+++ reverse.py
@@ -66,3 +66,14 @@ s = "JavaTpoint"
 print ("The original string  is : ",s)   
 print ("The reversed string using extended slice operator  is : ",reverse(s))  
 
+
+#reverse a string using reversed()   
+# Function to reverse a string   
+def reverse(str):   
+    string = "".join(reversed(str)) # reversed() function inside the join() function  
+    return string   
+  
+s = "JavaTpoint"  
+  
+print ("The original string is : ",s)   
+print ("The reversed string using reversed() is : ",reverse(s) )  
