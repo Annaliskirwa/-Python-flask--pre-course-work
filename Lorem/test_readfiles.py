45ce8b8016ca5374e6454dc5107bd70b00f0e8c1
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-20 18:18:11 +0300

    testing to see if we are actually getting data from the text file

--- Lorem/test_readfiles.py
+++ Lorem/test_readfiles.py
@@ -8,5 +8,14 @@ class TestReadFiles(unittest.TestCase):
     Args:
         unittest.TestCase: Class from the unittest module to create unit tests
     """
+
+def test_get_data(self):
+        """
+        Test case to confirm that we are getting data from the file
+        """
+        with open("test.txt","r") as handle:
+            data = handle.read()
+            self.assertEqual(data,readfiles.read_file("test.txt"))
+
 if __name__ == '__main__':
     unittest.main()
