cb07b2f98ee2b4143311461c581a0524044ebc8d
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-20 18:35:11 +0300

    nest the with operator inside a try block and we catch the FileNotFoundError exception we return None.

--- Lorem/readfiles.py
+++ Lorem/readfiles.py
@@ -3,4 +3,24 @@ def read_file(text_file):
     with open(text_file,"r") as handle:
         data = handle.read()
         print (data)
+
+
+def read_file(text_file):
+    """
+    Function that reads a text file and returns the data from the text file
+
+    Raises:
+        FileNotFoundError:If it cannot the file
+    """
+
+    try:
+        with open(text_file,"r") as handle:
+
+            data = handle.read()
+            return data
+    except FileNotFoundError:
+
+        return None
+
+        
 read_file(text_file)
