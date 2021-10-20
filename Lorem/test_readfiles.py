73548035038bb156087c42f9207213d2469f1b64
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-20 18:15:01 +0300

    It will set up the test class

---
+++ Lorem/test_readfiles.py
@@ -0,0 +1,12 @@
+import unittest
+import readfiles
+
+class TestReadFiles(unittest.TestCase):
+    """
+    Class to test the  functions on the  readfiles module
+
+    Args:
+        unittest.TestCase: Class from the unittest module to create unit tests
+    """
+if __name__ == '__main__':
+    unittest.main()
