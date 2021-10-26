365d5131d038de709aea3b4fc608a164aa855390
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-26 18:58:23 +0300

    HTML escaping

--- Quickstart/virtual/hello.py
+++ Quickstart/virtual/hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from markupsafe import escape
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '<h1>Hello world </h1>'
 
-if __name__ == '__main__':
-    app.run(debug = True)
+@app.route('/name')
+def hello(name):
+    return f"Hello, {escape(name)}!"
