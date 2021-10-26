d3460ff0131b0745416a58dfed01f7131d32ec6a
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-26 21:52:54 +0300

    It will connect the application to the configuration file

--- Watchlist/virtual/app/__init__.py
+++ Watchlist/virtual/app/__init__.py
@@ -4,5 +4,6 @@ from .config import Config, DevConfig
 app = Flask(__name__)
 #Setting up the configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
