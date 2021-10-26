df3480745e9d0608e04d4c78156ae2e4a6ea0b77
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-26 18:09:08 +0300

    It will initialize the quick start application

--- Watchlist/virtual/app/views.py
+++ Watchlist/virtual/app/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from flask import render_template
 from app import app
 #Views
@@ -7,3 +8,4 @@ def index():
     View root page function that returns the index page and its data
     """
     return render_template("index.html")
+
