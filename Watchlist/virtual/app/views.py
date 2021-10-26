002e2f42c6afc5275b6eb0448a7edbb1e4519b6d
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-26 20:35:09 +0300

    Changing the dynamic part in the route url to an integer

--- Watchlist/virtual/app/views.py
+++ Watchlist/virtual/app/views.py
@@ -12,9 +12,10 @@ def index():
     message = "Watchlist should be picked from this template : Hello"
     return render_template('index.html', message = message)
 
-@app.route('/movie/<movie_id>')
+@app.route('/movie/<int:movie_id>')
 def movie(movie_id):
-    """
+
+    '''
     View movie page function that returns the movie details page and its data
-    """
-    return render_template('movie.html', id = movie_id)
+    '''
+    return render_template('movie.html',id = movie_id)
