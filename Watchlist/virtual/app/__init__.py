004b198b01565e7394c71f6a0c14cdb265f93af3
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-26 21:59:35 +0300

    It will connect to the instance folder when the app is created

--- Watchlist/virtual/app/__init__.py
+++ Watchlist/virtual/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .config import Config, DevConfig
 #INitliazing our application
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 #Setting up the configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
