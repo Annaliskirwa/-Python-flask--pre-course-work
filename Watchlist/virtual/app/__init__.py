e738a332e47a1452c7928b4db5437acbeede1637
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-26 20:48:06 +0300

    It will set up the config in the init file

--- Watchlist/virtual/app/__init__.py
+++ Watchlist/virtual/app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from .config import Config, DevConfig
 #INitliazing our application
 app = Flask(__name__)
+#Setting up the configuration
+app.config.from_object(DevConfig)
+
 from app import views
