8eb6488f314fad96c221c1e15678348dc9499528
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-27 19:25:32 +0300

    It will make the API call and display in views

--- Watchlist/virtual/app/views.py
+++ Watchlist/virtual/app/views.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from app import app
+from .request import get_movies
 
 # Views
 @app.route('/')
@@ -9,9 +10,12 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    #Get a popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
     title = 'Home - Welcome to The best Movie Review Website Online'
     message = "Watchlist should be picked from this template : Hello"
-    return render_template('index.html', title = title, message= message)
+    return render_template('index.html', title = title, message= message, popular = popular_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
