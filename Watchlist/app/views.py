b229cce14b31a55d70513cc2d94c2701c35a4c6f
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-28 23:36:23 +0300

    WTF  forms

--- Watchlist/app/views.py
+++ Watchlist/app/views.py
@@ -4,6 +4,9 @@ from .requests import get_movies
 from .requests import get_movies,get_movie
 from .requests import get_movies,get_movie,search_movie
 from flask import render_template,request,redirect,url_for
+from .models import review
+from .forms import ReviewForm
+Review = review.Review
 
 # Views
 @app.route('/')
@@ -50,3 +53,17 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
