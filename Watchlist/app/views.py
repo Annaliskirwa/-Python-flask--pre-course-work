6d554b19e7da2f8f62c73ab250b6bb1e66b321b1
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-28 23:51:25 +0300

    It will update our html file to display existent movie reviews

--- Watchlist/app/views.py
+++ Watchlist/app/views.py
@@ -39,9 +39,10 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie)
-
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
+    
 @app.route('/search/<movie_name>')
 def search(movie_name):
     '''
