bc4c0a36fab57a851c53d2f5974d4296b6752c89
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-29 00:24:48 +0300

    It will configure requests

--- Watchlist/app/__init__.py
+++ Watchlist/app/__init__.py
@@ -15,5 +15,12 @@ def create_app(config_name):
     bootstrap.init_app(app)
 
     # Will add the views and forms
+# Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
     return app
