eda7d033e0dfd21ed4aa43db8a5a0254674202b6
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-28 22:57:04 +0300

    Requesting for an object

--- Watchlist/app/views.py
+++ Watchlist/app/views.py
@@ -3,9 +3,7 @@ from app import app
 from .requests import get_movies
 from .requests import get_movies,get_movie
 from .requests import get_movies,get_movie,search_movie
-
-
-
+from flask import render_template,request,redirect,url_for
 
 # Views
 @app.route('/')
@@ -19,9 +17,16 @@ def index():
     popular_movies = get_movies('popular')
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
-    title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
+    title = 'Home - Welcome to The best Movie Review Website Online'
+
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+        
 
 @app.route('/movie/<int:id>')
 def movie(id):
