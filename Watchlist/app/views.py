1a8925fc1477d68bb51ded99203f10103d5198d2
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-28 09:06:48 +0300

    It will add an error page

--- Watchlist/app/views.py
+++ Watchlist/app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
 from .requests import get_movies
+from .requests import get_movies,get_movie
+
 
 
 
@@ -20,11 +22,13 @@ def index():
     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
-    title = f'You are viewing {movie_id}'
-    return render_template('movie.html',title = title)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
