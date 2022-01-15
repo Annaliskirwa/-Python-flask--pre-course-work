549d1cbe38190dbe3af52916d5814b1cbcce681b
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-15 12:24:26 +0300

    Few changes on the script

--- WordOccurence/wordoccurence.py
+++ WordOccurence/wordoccurence.py
@@ -1,12 +1,12 @@
 #! /usr/bin/python
 
-text = open("sample.tyxt", "r")
+text = open("sample.txt", "r")
 
 d = dict()
 
 for line in text:
     line = line.strip()
-    line - line.lower()
+    line = line.lower()
     words = line.split()
 
     for word in words:
@@ -15,5 +15,5 @@ for line in text:
         else:
             d[word] = 1
 
-for key in list(d.keys):
+for key in list(d.keys()):
     print(key, ":", d[key])
