cb0f2fcbb6872a2ee2de83e591e1a4bb46de1d68
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-15 12:20:51 +0300

    Added the script file for counting the word occurence

---
+++ WordOccurence/wordoccurence.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+text = open("sample.tyxt", "r")
+
+d = dict()
+
+for line in text:
+    line = line.strip()
+    line - line.lower()
+    words = line.split()
+
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+
+for key in list(d.keys):
+    print(key, ":", d[key])
