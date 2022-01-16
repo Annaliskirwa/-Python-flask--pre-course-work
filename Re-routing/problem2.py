833da886dfff104439445b723603f0cbb5daad6a
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-16 11:44:31 +0300

    Send some data

--- Re-routing/problem2.py
+++ Re-routing/problem2.py
@@ -2,5 +2,8 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-## Connect to an IP with Port or url
+#Connect to an IP with Port or url
 sock.connect(('0.0.0.0', 8080))
+
+#Send some data
+sock.send("Twenty-five bytes to send")
